8274eece721db46cd8fcce7e54b5932696a40905
PouyaFakhri <pouyaf77.pf@gmail.com>
2025-09-12 18:23:55 +0330

    edit authprovider

--- src/providers/AuthProvider.js
+++ src/providers/AuthProvider.js
@@ -10,15 +10,8 @@ function AuthProvider({ children }) {
   const { data, isPending } = UseGetUserProfile();
 
   useEffect(() => {
-    if (!data?.data && !isPending) router.push("/");
+    if (!data && !isPending) router.push("/");
   }, [isPending]);
-
-  if (isPending)
-    return (
-      <p className=" m-auto bg-blue-100 text-blue-500 w-fit animate-ping">
-        Loading...
-      </p>
-    );
 
   return children;
 }
